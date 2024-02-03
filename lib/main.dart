aceacfb7b6bbf0190dc5f593faeb1534269880ab
unknown <niranjanmramesh@gmail.com>
2024-02-03 16:19:56 +0530

    new place

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,13 @@ import 'package:wander_in/account1.dart';
 import 'package:wander_in/blogs.dart';
 import 'package:wander_in/home1.dart';
 import 'package:wander_in/login1.dart';
+import 'package:wander_in/map.dart';
 import 'package:wander_in/notification.dart';
+import 'package:wander_in/placedetails.dart';
+import 'package:wander_in/plans.dart';
 import 'package:wander_in/posts.dart';
 import 'package:wander_in/review.dart';
+import 'package:wander_in/search.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,7 +30,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: blogs(),
+      home: placedetailState(),
     );
   }
 }
