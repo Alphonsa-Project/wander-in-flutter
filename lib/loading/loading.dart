6ae9e068212974b9d5aba5501e8a6896a60a36f4
_abi <72296086+Abjithwyn@users.noreply.github.com>
2024-03-02 14:17:05 +0530

    Update loading.dart

--- lib/loading/loading.dart
+++ lib/loading/loading.dart
@@ -30,3 +30,32 @@ class Loading extends StatelessWidget {
     );
   }
 }
+
+
+// FirestoreQueryBuilder<Map<String, dynamic>>(
+//   query: FirebaseFirestore.instance.collection('user'),
+//   builder: (context, snapshot, _) {
+//     // ...
+
+//     return GridView.builder(
+//       itemCount: snapshot.docs.length,
+//       itemBuilder: (context, index) {
+//         // if we reached the end of the currently obtained items, we try to
+//         // obtain more items
+//         if (snapshot.hasMore && index + 1 == snapshot.docs.length) {
+//           // Tell FirestoreQueryBuilder to try to obtain more items.
+//           // It is safe to call this function from within the build method.
+//           snapshot.fetchMore();
+//         }
+
+//         final user = snapshot.docs[index].data();
+
+//         return Container(
+//           padding: const EdgeInsets.all(8),
+//           color: Colors.teal[100],
+//           child: const Text("User name is ${user['name']}"),
+//         );
+//       },
+//     );
+//   },
+// );
